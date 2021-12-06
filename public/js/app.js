580c80f2e45048d2a35a7412aea85059a59280e9
Anshul Chouhan <anshulchouhan2401@gmail.com>
2021-12-06 11:11:17 +0530

    remove unnecessary comsole.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,3 @@
-console.log('client side javascript  is loaded')
 const weatherform =document.querySelector('form')
 const search=document.querySelector('input')
 const messageOne=document.querySelector('#message-1')
